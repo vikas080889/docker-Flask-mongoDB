ef5695f45410d6936605234bd8fe5ff50ec1f274
root <vikassharma.ait@gmail.com>
2019-09-22 20:30:52 +0530

    adding new apis

--- web/app.py
+++ web/app.py
@@ -9,6 +9,7 @@ api = Api(app)
 
 client = MongoClient("mongodb://db:27017")
 db = client.stocks
+db2 = client.bseStocks
 
 class TotalStocks(Resource):
     def get(self):
@@ -18,7 +19,8 @@ class TotalStocks(Resource):
 
 class MidCaps(Resource):
     def get(self):
-        mid_stocks = dumps(db.mojoanalysis.find({"$text":{"$search": "\"Mid Cap\"" }}))
+        mid_stocks = dumps(db.mojoanalysis.find({"$text":
+        {"$search": "\"Mid Cap\"" }}))
         mid_stocks = json.loads(mid_stocks)
         count = len(mid_stocks)
         ret_map = {
@@ -32,7 +34,8 @@ class StockInfo(Resource):
         posted_data = request.get_json()
         stock_name = posted_data["stock"]
         stock_name = '"{}"'.format(stock_name)
-        stocks_data = json.loads(dumps(db.mojoanalysis.find({ "$text": { "$search": stock_name}})))
+        stocks_data = json.loads(dumps(db.mojoanalysis.find({ "$text":
+        { "$search": stock_name}})))
         return stocks_data
 
 class QualityFilter(Resource):
@@ -75,11 +78,38 @@ class QualityFilterSelectedField(Resource):
           stocks = json.loads(stocks)
           return stocks
 
+class StocksFromKyc(Resource):
+      def post(self):
+          posted_data = request.get_json()
+          kyc_data = posted_data['kyc_data']
+           #quality = posted_data['quality']
+          stocks = dumps(db.mojoanalysis.find(
+                      {"$text": {"$search": kyc_data}},{"URL":1}))
+          stocks = json.loads(stocks)
+          return stocks
+
+class BSESustainedProfitGain(Resource):
+    def get(self):
+        stocks = dumps(db2.stockreposts.aggregate([{"$match":
+        {"$and" :[{"$expr":{"$gt":["$q1_profit", "$q2_profit"]}},
+        {"$expr":{"$gt":["$q2_profit", "$q3_profit"]}},
+        {"$expr":{"$gt":["$q3_profit", "$q4_profit"]}}  ]}}]))
+        stocks = json.loads(stocks)
+        count = len(stocks)
+        ret_map = {
+            "total":count,
+            "stocks": stocks
+        }
+        return jsonify(ret_map)
+
+
 api.add_resource(TotalStocks, "/total")
 api.add_resource(MidCaps, "/mid_caps")
 api.add_resource(StockInfo,"/stock_info")
 api.add_resource(QualityFilter,"/quality_filter")
 api.add_resource(QualityFilterSelectedField,"/quality_filter_selected")
+api.add_resource(StocksFromKyc,"/kyc_filter")
+api.add_resource(BSESustainedProfitGain,"/bse_sustained_profit_gain")
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", debug=True)
